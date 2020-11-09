a90f0c273aa155de3988f0aa963a5be27a53be12
vishivishvish <vishnu.subramanian93@gmail.com>
2020-11-10 01:58:16 +0530

    Initial CLI Commit

---
+++ append.py
@@ -0,0 +1,19 @@
+import pandas as pd;
+from glob import glob;
+
+files = glob('Data/*.csv');
+full_df = None;
+
+for f in files:
+    print(f);
+    df = pd.read_csv(f);
+
+    symbol = f.split(r'"\"'[1])[1].split('.')[0];
+    df['Name'] = symbol;
+
+    if full_df is None:
+        full_df = df;
+    else:
+        full_df = full_df.append(df, ignore_index=True);
+
+full_df.to_csv('sp500full.csv', index=False);
